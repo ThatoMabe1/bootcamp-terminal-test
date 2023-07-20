a63b2e0a62e8abef4e2b9b3270ce4e93b3ddad39
Thato Mabe <willynough@gmail.com>
2023-07-20 14:30:35 +0200

    first commit

---
+++ Test/finditemsOver20.test.js
@@ -0,0 +1,14 @@
+import assert from "assert";
+import findItemsOver20 from "../findItemsOver20.js";
+
+describe('testing my findItemsOver20 function', function(){
+    it('The function should return all the products that have a quantity higher than 20.' , function(){
+        var itemList = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+            {name : 'apples', qty : 3},
+        ];        
+});
+
+});
